22b7aca775ee297d338a70e666f57dbd5a549231
Marco <marcogough07@gmail.com>
2025-01-26 17:06:37 -0500

    Initial Commit

---
+++ FIITFit/FIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIITFitApp.swift
+//  FIITFit
+//
+//  Created by Student on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FIITFitTests/FIITFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FIITFitTests.swift
+//  FIITFitTests
+//
+//  Created by Student on 1/26/25.
+//
+
+import Testing
+@testable import FIITFit
+
+struct FIITFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
